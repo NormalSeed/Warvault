322c7b433c539885751f1b8b06ea8377e5752784
NormalSeed <dlrjsghk11@naver.com>
2025-12-26 03:44:37 +0900

    [Feat] Pause Game
    GameManager에 게임 일시정지 로직 구현

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+
+    public bool isMenuOpen = false;
 
     void Awake()
     {
@@ -16,5 +16,25 @@ public class GameManager : MonoBehaviour
         Instance = this;
     }
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseControll();
+        }
+    }
 
+    void PauseControll()
+    {
+        isMenuOpen = !isMenuOpen;
+
+        if (isMenuOpen)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+        }
+    }
 }
