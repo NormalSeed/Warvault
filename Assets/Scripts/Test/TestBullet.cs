cb0df1317d13399c899e90d13f43ee8f5d7d8413
NormalSeed <dlrjsghk11@naver.com>
2025-12-29 10:53:16 +0900

    [Feat] TakeDamage & Die Logic

--- Assets/Scripts/Test/TestBullet.cs
+++ Assets/Scripts/Test/TestBullet.cs
@@ -3,6 +3,7 @@
 public class TestBullet : PooledObject
 {
     [SerializeField] float lifeTime = 2.0f;
+    [SerializeField] int damage = 10;
     float timer;
 
     public override void OnSpawn()
@@ -11,11 +12,25 @@ public class TestBullet : PooledObject
         GetComponent<Rigidbody>().velocity = transform.forward * 10f;
     }
 
-    private void Update()
+    void Update()
     {
         timer += Time.deltaTime;
         if (timer >= lifeTime)
         {
+            ReturnPool();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            EnemyController enemy = other.GetComponent<EnemyController>();
+
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
             ReturnPool();
         }
     }
